50210b7e164cf539b2a8f2652770fc20aece1c57
Devbriandurand <durand.brian33@gmail.com>
2020-03-06 12:38:19 +0100

    Modification margin bottom page administration

--- view/frontend/administration.php
+++ view/frontend/administration.php
@@ -1,5 +1,5 @@
 <?php ob_start(); ?>
 <h1 id='titleAdmin'>BIENVENUE DANS L'ADMINISTRATION</h1>
-
+<!-- COMMENTAIRE SIGNALER -->
 <?php $content = ob_get_clean(); ?>
 <?php require('view/frontend/template.php'); ?>
