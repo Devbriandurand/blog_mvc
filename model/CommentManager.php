f118aab4c5b83db60c07dc9ef5dfc7997c67ba76
Devbriandurand <durand.brian33@gmail.com>
2020-03-10 18:46:50 +0100

    Adds functionality

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -1,12 +1,11 @@
 <?php
-
 namespace Brian\Blog\Model;
-
 require_once("model/Manager.php");
 
 class CommentManager extends Manager
 {
-    public function getComments($postId)
+//Selectionne les commentaires
+    public function getComment($postId)
     {
         $db = $this->dbConnect();
         $comments = $db->prepare('SELECT id, post_id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
@@ -16,46 +15,88 @@ class CommentManager extends Manager
     }
 
         public function commentsGet()
-    {
+    {   
         $db = $this->dbConnect();
-        $req = $db->query('SELECT * FROM comments ');
-
+        $req = $db->query('SELECT * FROM comments ORDER BY signalement DESC ');//Order by afin que les signalement soit en premier
         return $req;
     }
-    
+
+//Post les commentaires
     public function postComment($postId, $author, $comment)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+        $comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date, signalement) VALUES(?, ?, ?,NOW(), "non" )');
         $affectedLines = $comments->execute(array($postId, $author, $comment));
 
         return $affectedLines;
     }
 
-    // Signaler un commentaire.
-    public function reportComment($id) {
-        $db = $this->newManager->dbConnect();
-        $request = $db->prepare('UPDATE comments SET alert = 1 WHERE id = ?');
-        $request->execute(array($id));
+    
+//SIGNALER UN COMMENTAIRE
+    public function signalManagerComment($CommentId)
+    {  
+        $db = $this->dbConnect();
+        $comments = $db->prepare('UPDATE comments SET signalement = "oui" WHERE id = ?');
+        $comments->execute(array($CommentId));
     }
 
-    //Editer un commentaire
-    public function editComment($postId, $author, $comment)
-    {
+    
+// DESIGNALER UN COMMENTAIRE
+    public function alertComment($CommentId)
+    {  
         $db = $this->dbConnect();
-        $request = $db->prepare('UPDATE comments SET comment= ?, comment_date = NOW() WHERE id =?');
-        $comment = $request->execute(array($comment, $id));
-        //resultat
-        return $comment;
+        $comments = $db->prepare('UPDATE comments SET signalement = "non" WHERE id = ?');
+
+        $comments->execute(array($CommentId));
+
     }
-    //Supprimer un commentaire
-    public function deleteComment($id)
-    {
+
+//SUPPRIMER UN COMMENTAIRE
+    public function deleteComment($CommentId)
+    {  
         $db = $this->dbConnect();
-        $req = $db->prepare('DELETE FROM comments WHERE id = ?');
-        $delete = $req->execute(array($id));
-        return $supprime;
+        $comments = $db->prepare('DELETE FROM comments WHERE id = ?');
+
+        $comments->execute(array($CommentId));
     }
+    
+
+//SUPPRIMER UN CHAPITRE
+    public function deleteChapter($ChapterId)
+    {  
+        $db = $this->dbConnect();
+        $chapters = $db->prepare('DELETE FROM posts WHERE id = ? LIMIT 1');
+
+        $chapters->execute(array($ChapterId));
+    }
+
+//SUPPRIMER UN USER
+    public function deleteUser($userId)
+    {  
+        $db = $this->dbConnect();
+        $users = $db->prepare('DELETE FROM users WHERE id = ? LIMIT 1');
+
+        $users->execute(array($userId));
+    }
+
+
+
+    // // Signaler un commentaire.
+    // public function reportComment($id) {
+    //     $db = $this->newManager->dbConnect();
+    //     $request = $db->prepare('UPDATE comments SET alert = 1 WHERE id = ?');
+    //     $request->execute(array($id));
+    // }
+
+    // //Editer un commentaire
+    // public function editComment($postId, $author, $comment)
+    // {
+    //     $db = $this->dbConnect();
+    //     $request = $db->prepare('UPDATE comments SET comment= ?, comment_date = NOW() WHERE id =?');
+    //     $comment = $request->execute(array($comment, $id));
+    //     //resultat
+    //     return $comment;
+    // }
 
 
 }
