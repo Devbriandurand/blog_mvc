c470cb6b176cf7ddc3ca188bd58bb05c328f2d85
Devbriandurand <durand.brian33@gmail.com>
2020-03-10 18:38:49 +0100

    add fonction addChapter & function updateChapter

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace Brian\Blog\Model;
-
 require_once("model/Manager.php");
 
 class PostManager extends Manager
@@ -23,4 +21,28 @@ class PostManager extends Manager
 
         return $post;
     }
+//Requete permettant d'inserer une ligne dans la table post(chapitre)
+    public function addChapter($title, $content)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('INSERT INTO posts (title, content, creation_date) VALUE (?,?,NOW())');
+        $req->execute(array($title, $content));
+    }
+
+
+//Requete permettant de modifier et d'inserer le chapitre éditer
+        public function updateChapter($postId, $chapTitle, $chapContent)
+    {
+        $db = $this->dbConnect();
+        $req = $db->prepare('UPDATE posts SET title = :title, content= :content WHERE id = :id');
+        $req->execute(array(':id'=> $postId, ':title' => $chapTitle,':content' => $chapContent));
+        return $req;
+    }
+
 }
+
+
+
+
+        // $req = $db->prepare('UPDATE posts SET title = "?", content="?" WHERE id = ?');
+        // $req->execute(array( $chapTitle, $chapContent, $postId));
