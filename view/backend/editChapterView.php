df313a38c03f8b12e38e5c0ae132b75d3d77ca52
Devbriandurand <durand.brian33@gmail.com>
2020-03-10 18:51:55 +0100

    Adding a new page new chapter

---
+++ view/backend/editChapterView.php
@@ -0,0 +1,43 @@
+
+<?php ob_start(); ?>    
+
+<h1 id="titleEditChapter">PAGE ADMIN <br/> EDITION DE CHAPITRE</h1>
+   
+   <form id="formulaireAddChapter" action="index.php?action=editChapter&amp;id=<?= $chapter['id']; ?>" method="post">
+
+                <fieldset  id="fieldsetAddChap">
+                    <legend align="center" id="legendFormPost">MODIFICATION DE BILLET</legend>
+                        <table>
+                            <tr>
+                                <td id="tdAddChapter">
+                                    <label for="title"><strong>TITRE DU CHAPITRE</strong></label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="tdAddChapter" align='center'>
+                                    <input name="title" value="<?= $chapter['title'] ?>"></input>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="tdAddChapter">
+                                    <label for="content"><strong>CONTENUE DU CHAPITRE</strong></label>        
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="tdAddChapter">
+                                    <textarea class="tinymce" name="content" rows="20" cols="60" >
+                                      <?= ($chapter['content']); ?>
+                                    </textarea>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="tdSumbitAddChapter" align='center'>
+                                    <input id="sumbitAddChapter" type="submit" value="Envoyer">
+                                </td>
+                            </tr>
+                    </table>
+                </fieldset>
+        </form>
+
+ <?php $content = ob_get_clean(); ?>
+<?php require('view/frontend/template.php'); ?>
