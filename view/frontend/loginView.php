cd43cd951f9c45028474b19be5f788bb0b5e740e
Devbriandurand <durand.brian33@gmail.com>
2020-03-11 16:06:45 +0100

    LoginView Main connexion, modification of the form + indentation

--- view/frontend/loginView.php
+++ view/frontend/loginView.php
@@ -1,41 +1,42 @@
-<!DOCTYPE html>
-<html>
+<?php ob_start(); ?>
+<?php $title = 'titleLogin' ?>
 
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title></title>
-</head>
-
-<body>
-
-
-    <?php ob_start(); ?>
     <h1 id="titleLogin">PAGE DE CONNEXION</h1>
-
     <form class="formLogin" action="index.php?action=connexion" method="POST">
-
         <fieldset class="fieldsetLogin">
             <legend id="legendLogin" align='center'><strong>Connexion</strong></legend>
-                <label for="username"><strong>Login</strong></label>
-                <input id="inputLogin" type="text" name="username">
-                    <label><strong>Mot de passe</strong></label>
-                    <input id="inputLogin" type="password" name="mdp">
-                    <input id="inputLoginSubmit" type="submit" name="submitConnexion">
-                    <div class=containerLinkRegister>
-                        <a href="index.php?action=inscription">
-                            <strong>Vous n'êtes pas encore membre ?</strong>
-                        </a>
+                <table id="tableLogin">
+                    <tbody class="tbodyLogin">
+                        <tr class="containerLoginTr">
+                                
+                            <th>
+                                <label for="username"><strong>LOGIN</strong></label>
+                            <th>
+                                <td>     
+                                    <input id="inputLogin" type="text" name="username">
+                                </td>
+                            <th>
+                             <label><strong>MOT DE PASSE</strong></label>
+                            </th>
+                                <td>
+                                    <input id="inputLogin" type="password" name="mdp">
+                                </td>
+                                <td class="containerSubmitRegister">
+                                    <input id="inputLoginSubmit" type="submit" name="submitConnexion">
+                                </td> 
+                            </tr>
+                    </tbody>
+                </table>
+                    <div class="containerLinkRegister">
+                            <a href="index.php?action=inscription">
+                                <strong>Vous n'êtes pas encore membre ?</strong>
+                            </a>
                     </div>
-        </fieldset>
-                    
+        </fieldset> 
     </form>
-    <div id="containerImgLogin"><img id="imgLogin" src="public/images/imgAlaskas.jpg"></div>
-    <?php $content = ob_get_clean(); ?>
 
+                    <div id="containerImgLogin"><img id="imgLogin" src="public/images/imgAlaskas.jpg"></div>
 
+<?php $content = ob_get_clean(); ?>
 
-    <?php require('template.php'); ?>
-</body>
-
-</html>
+<?php require('template.php'); ?>
