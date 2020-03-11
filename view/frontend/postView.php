220f6e2406ed9c8f00c7b453a81bb2c365f6e8a0
Devbriandurand <durand.brian33@gmail.com>
2020-03-11 16:17:49 +0100

    PostView adding an id to the table, tbody, td then adding contitions to display the comments.

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,5 +1,7 @@
 <?php ob_start(); ?>
 
+<?php $title = 'titlePostView' ?>
+
 <h1 id="titlePostView">Mes chapitres !</h1>
 
 <p><a id="retourBillet" href="index.php">Retour à la liste des billets</a></p>
@@ -13,7 +15,6 @@
     <p id="content">
         <?= nl2br(htmlspecialchars($post['content'])) ?>
     </p>
-
 </div>
 
 <h2 id="titleComment">Commentaires</h2>
@@ -23,32 +24,34 @@
 <form id="formulaireComment" action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
     <fieldset id="fieldsetPost">
         <legend align="center" id="legendFormPost">LAISSER MOI VOTRE AVIS</legend>
-        <table>
-        <tr>
-            <td>
-                <label for="author"><strong>Auteur</strong></label>
-            </td>
-         </tr>
-        <tr>
-            <td>
-                <input size="79" type="text" name="author" placeholder="Pseudo">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <label for="comment"><strong>Commentaire</strong></label>        
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <textarea id="comment" name="comment" placeholder="Vous n'alliez tout de même pas partir sans me laisser un commentaire ?! ^_^" rows="12" cols="60"></textarea>
-            </td>
-        </tr>
-        <tr>
-            <td align='center'>
-                <input type="submit" value="Envoyer">
-            </td>
-        </tr>
+            <table id="tablePostCom">
+                <tbody id="tbodyPostView">
+            <tr>
+                <td>
+                    <label for="author"><strong>Auteur</strong></label>
+                </td>
+            </tr>
+            <tr>
+                <td align='center'>
+                    <input id="pseudoPostView" type="text" name="author" placeholder="Pseudo">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="comment"><strong>Commentaire</strong></label>        
+                </td>
+            </tr>
+            <tr>
+                <td id="textareaPostView">
+                    <textarea id="comment" name="comment" placeholder="Vous n'alliez tout de même pas partir sans me laisser un commentaire ?! ^_^" rows="15" cols="75"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td align='center'>
+                    <input type="submit" value="Envoyer">
+                </td>
+            </tr>
+        </tbody>
     </table>
     </fieldset>
 </form>
@@ -59,8 +62,14 @@
 while ($comment = $comments->fetch())
 {
 ?>
-    <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr']   ?></p>
-    <p><?= nl2br(htmlspecialchars($comment['comment'])) . '<br/>' .' <span><input type=button value="signaler" </span>' .'<hr>'  ?></p>
+    <p>
+        <strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?>
+    </p>
+    <p>
+        <?= nl2br(htmlspecialchars($comment['comment']))?>
+        <a href="index.php?action=signalerCom&amp;id=<?= $comment['id'] ?>">Signaler</a>
+        <hr>
+    </p>
 <?php
 }
 ?>
