18bb38b5beb5bcf6e9a8a23992b5bbd2503d137b
Devbriandurand <53628729+Devbriandurand@users.noreply.github.com>
2020-03-18 20:35:19 +0100

    Update Manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -6,7 +6,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=localhost;dbname=bdd_posts;charset=utf8', 'root', 1234);
+        $db = new \PDO('mysql:host=localhost;dbname=bdd_posts;charset=utf8', '', );
         return $db;
     }
 }
